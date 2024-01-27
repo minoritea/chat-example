d64d913503bea5f51d98edec0672fb8fd08df23c
Minori Tokuda <snowcrush@mail.0pt.jp>
2024-01-27 23:25:13 +0900

    step4

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"embed"
 	"html/template"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -11,9 +13,31 @@ import (
 //go:embed template/*.html
 var fs embed.FS
 
-var messages []string
+type Message struct {
+	ID   string
+	Body string
+}
+
+var messages []Message
+var messageID int
 
 func GetIndex(w http.ResponseWriter, r *http.Request) {
+	if strings.Contains(r.Header.Get("Accept"), "text/vnd.turbo-stream.html") {
+		w.Header().Set("Content-Type", "text/vnd.turbo-stream.html")
+		lastMessageID := r.FormValue("lastMessageId")
+		messages := messages
+		if lastMessageID != "" {
+			for i, message := range messages {
+				if message.ID == lastMessageID {
+					messages = messages[i+1:]
+					break
+				}
+			}
+		}
+		t, _ := template.ParseFS(fs, "template/messages.html")
+		t.Execute(w, map[string]any{"Messages": messages})
+		return
+	}
 	t, _ := template.ParseFS(fs, "template/index.html")
 	t.Execute(w, map[string]any{"Messages": messages})
 }
@@ -21,8 +45,17 @@ func GetIndex(w http.ResponseWriter, r *http.Request) {
 func PostMessage(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	message := r.FormValue("message")
-	messages = append(messages, message)
-	http.Redirect(w, r, "/", http.StatusFound)
+	lastMessageID := r.FormValue("lastMessageId")
+	messageID++
+	messages = append(messages, Message{
+		ID:   "message-" + strconv.Itoa(messageID),
+		Body: message,
+	})
+	redirectTo := "/"
+	if lastMessageID != "" {
+		redirectTo = "/?lastMessageId=" + lastMessageID
+	}
+	http.Redirect(w, r, redirectTo, http.StatusFound)
 }
 
 func main() {
